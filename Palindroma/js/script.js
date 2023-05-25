1a3cd5a3cf7f39f8f39f9fa860fec05c502c65c4
SimoneVilardo <simovila95@gmail.com>
2023-05-25 14:27:09 +0200

    Esercizio Palindroma finito

---
+++ Palindroma/js/script.js
@@ -0,0 +1,24 @@
+// VARIABILE CHE PERMETTE ALL' UTENTE DI INSERIRE UNA PAROLA
+
+let parola_utente = prompt('Inserisci una parola').toLowerCase();
+
+// FUNZIONE SPLIT
+ let parola_array = parola_utente.split('');
+ console.log(parola_array);
+
+//  FUNZIONE REVERSE
+let parola_contrario = parola_array.reverse();
+console.log(parola_contrario);
+
+// FUNZIONE JOIN
+let contrario = parola_contrario.join('');
+console.log(contrario);
+
+// CONDIZIONE
+
+if (contrario === parola_utente){
+    console.log('La parola è PALINDOMA')
+}
+else {
+    console.log('La parola NON è PALINDOMA')
+}
